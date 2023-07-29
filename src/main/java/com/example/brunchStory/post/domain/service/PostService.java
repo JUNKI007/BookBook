5c11b08bd30900a6af8fd35a2cbb4630df09066d
gotoAbraxas <lty7085@gmail.com>
2023-07-29 23:34:43 +0900

    PostStart

---
+++ src/main/java/com/example/brunchStory/post/domain/service/PostService.java
@@ -0,0 +1,11 @@
+package com.example.brunchStory.post.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+
+}
