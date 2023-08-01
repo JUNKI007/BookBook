a2ac8247e4c6b01cc99cd91d26b43ba7bea27dff
1996svibe <dndwlscjwo87@gmail.com>
2023-08-01 16:44:50 +0900

    좋아요 서비스 수정

--- src/main/java/com/example/brunchStory/post/service/LikesService.java
+++ src/main/java/com/example/brunchStory/post/service/LikesService.java
@@ -1,14 +1,41 @@
 package com.example.brunchStory.post.service;
 
 import com.example.brunchStory.member.domain.entity.Member;
+import com.example.brunchStory.post.domain.entity.Likes;
 import com.example.brunchStory.post.domain.entity.Post;
+import com.example.brunchStory.post.repository.LikesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface LikesService {
-    void like(Member member, Post post);
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikesService {
+    private final LikesRepository likesRepository;
+    private final PostService postService;
+    public void like(Long memberId, Long postId){
+        Member member = Member.builder().id(memberId).build();
+        Post post = postService.findById(postId);
+        Likes likes = new Likes();
+        likes.setMember(member);
+        likes.setPost(post);
+        likesRepository.save(likes);
+    }
+    public void  unlike(Long memberId, Long postId){
+        Member member = Member.builder().id(memberId).build();
+        Post post = postService.findById(postId);
+        Likes likes =  likesRepository.findByMemberAndPost(member, post);
+    }
 
-    void  unlike(Member member, Post post);
+    public boolean hasLiked(Long memberId, Long postId){
+        Member member = Member.builder().id(memberId).build();
+        Post post = postService.findById(postId);
+        return likesRepository.existsByMemberAndPost(member, post);
+    }
 
-    boolean hasLiked(Member member, Post post);
-
-    int getLikeCount(Post post);
+    public Long getLikeCount(Long postId){
+        Post post = postService.findById(postId);
+        return likesRepository.countByPost(post);
+    }
 }
