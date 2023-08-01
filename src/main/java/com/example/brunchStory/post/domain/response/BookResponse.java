9a1cbbc201b9640ac959576d6dafa74e4a0eb594
JUNGI JEONG <junejjk@gmail.com>
2023-08-01 16:56:03 +0900

    #11 Token에서 Role 정보를 읽어 Security 기능 적용

--- src/main/java/com/example/brunchStory/post/domain/response/BookResponse.java
+++ src/main/java/com/example/brunchStory/post/domain/response/BookResponse.java
@@ -1,20 +1,20 @@
-package com.example.brunchStory.post.domain.response;
-
-import com.example.brunchStory.config.domain.dto.BookDto;
-import com.example.brunchStory.config.domain.dto.PostDto;
-import com.example.brunchStory.post.domain.entity.Book;
-import com.example.brunchStory.post.domain.entity.Publish;
-
-import java.util.List;
-
-public class BookResponse extends BookDto {
-
-    private MemberDto memberDto;
-    private List<PostDto> postDtos;
-
-    public BookResponse(Book book){
-        super(book);
-        this.memberDto = new MemberDto(book.getAuthor());
-        this.postDtos = book.getPublishedPosts().stream().map(PostDto::new).toList();
-    }
-}
+//package com.example.brunchStory.post.domain.response;
+//
+//import com.example.brunchStory.config.domain.dto.BookDto;
+//import com.example.brunchStory.config.domain.dto.PostDto;
+//import com.example.brunchStory.post.domain.entity.Book;
+//import com.example.brunchStory.post.domain.entity.Publish;
+//
+//import java.util.List;
+//
+//public class BookResponse extends BookDto {
+//
+//    private MemberDto memberDto;
+//    private List<PostDto> postDtos;
+//
+//    public BookResponse(Book book){
+//        super(book);
+//        this.memberDto = new MemberDto(book.getAuthor());
+//        this.postDtos = book.getPublishedPosts().stream().map(PostDto::new).toList();
+//    }
+//}
